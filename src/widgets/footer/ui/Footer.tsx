cf03fb06378ce2e7be919277c728c43aa3ea8bdc
Aleksey Popov <alekseypopov.dev@gmail.com>
2024-05-16 05:27:25 +0700

    feat: create footer

---
+++ src/widgets/footer/ui/Footer.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Footer: FC = () => {
+  return (
+    <footer>
+      <div className="container">Footer</div>
+    </footer>
+  );
+};
+
+export default Footer;
