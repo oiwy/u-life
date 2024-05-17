4fc28bbd75a53ccc3eb59853d5a487fb719fb1b2
Aleksey Popov <alekseypopov.dev@gmail.com>
2024-05-17 23:39:11 +0700

    feat: update Layout.tsx

--- src/widgets/layout/ui/Layout.tsx
+++ src/widgets/layout/ui/Layout.tsx
@@ -5,13 +5,13 @@ import { Footer, Header } from "@/widgets";
 
 const Layout: FC = () => {
   return (
-    <>
+    <div className="flex flex-col justify-between gap-6">
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
