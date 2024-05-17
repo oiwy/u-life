e11cde9acee2472a6dd0b95b95a63999f834148f
Aleksey Popov <alekseypopov.dev@gmail.com>
2024-05-17 23:38:38 +0700

    feat: update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,20 @@ export default {
   theme: {
     container: {
       center: true,
+      padding: {
+        DEFAULT: "1.25rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+    colors: {
+      black: "#070d08",
+      gray: "#e2f3eb",
+      "gray-2": "#d3edee",
+      white: "#f0f9f9",
+      blue: "#ade1e5",
+      green: "#99d19c",
     },
     extend: {},
   },
